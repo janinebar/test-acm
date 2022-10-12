e0d3e5a8dd498650332ec3973dbed4dc7835d102
Rajas Nathak <61021680+rajasnathak@users.noreply.github.com>
2022-10-11 22:00:59 -0500

    Update findselector.py

--- findselector.py
+++ findselector.py
@@ -1,21 +1,23 @@
+selectorList = {"continent": "asia", "loc" : "east"}
+selects = []
+
 def find_selector(selectorList):
-    selectorList = "continent: 10"
-    print(selectorList)
-    res_str = selectorList.partition(":")[2]
-    print(res_str.strip())
-    selects = []
-    if (selectorList.partition(":")[0]=="loc"):
-        selections = "abm-" + res_str.strip() + "-sel"
-        print(selections)
-        selects.append(selections)
+    for key in selectorList:
+        print(selectorList)
+        res_str = selectorList.get(key).strip()
+        print(selectorList.get(key))
+        if (key=="loc"):
+            selections = "abm-" + res_str + "-sel"
+            print(selections)
+            selects.append(selections)
+        elif(key=="canary"):
+            selections = "canary-" + res_str + "-sel"
+            print(selections)
+            selects.append(selections)
+        else:
+            selections = "continent-" + res_str + "-sel"
+            print(selections)
+            selects.append(selections)
 
-    elif(selectorList.partition(":")[0]=="canary"):
-        selections = "canary-" + res_str.strip() + "-sel"
-        print(selections)
-        selects.append(selections)
-    else:
-        selections = "continent-" + res_str.strip() + "-sel"
-        print(selections)
-        selects.append(selections)
-
-    print(selects)
+find_selector(selectorList)
+print(selects)
